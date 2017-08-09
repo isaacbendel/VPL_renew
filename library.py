00c739c50a0c5f2cdf41b19dfe893f5836218011
bendel <bendel@localhost.localdomain>
2017-08-09 18:17:08 -0400

    Initial Commit

---
+++ library.py
@@ -0,0 +1,57 @@
+PATH_TO_CHROMEDRIVER = '/home/bendel/Downloads/chromedriver'
+LOGIN_INFO = [('LIBRARYCARD1','PIN1'),('LIBRARYCARD2','PIN2')]
+PATH_FOR_SCREENSHOTS = '/home/bendel/Pictures'
+TIME_FACTOR = 1
+
+from selenium import webdriver
+import time
+import datetime
+import os
+
+
+def login(browser):
+    time.sleep(1)
+    username_box = browser.find_element_by_xpath('//input[@name="name"]')
+    username_box.clear()
+    username_box.send_keys(login_info[0])
+    password_box = browser.find_element_by_xpath('//input[@name="user_pin"]')
+    password_box.clear()
+    password_box.send_keys(login_info[1])
+    login_button = browser.find_element_by_xpath('//input[@name="commit"]')
+    time.sleep(0.5)
+    login_button.click()
+    time.sleep(1)
+
+def renew(browser):
+    browser.get('https://vaughanpl.bibliocommons.com/checkedout/index/out')
+    time.sleep(4)
+    select_all_button = browser.find_element_by_xpath('//span[@class="check"]')
+    select_all_button.click()
+    time.sleep(1)
+    renew_button = browser.find_element_by_xpath('//a[@data-action="/checkedout/renew_confirmation"]')
+    renew_button.click()
+    time.sleep(5)
+    filename_for_screenshot = os.path.join(PATH_FOR_SCREENSHOTS,'Renewal_for_account_' + str(login_info[0]) + '_on_' + datetime.datetime.today().strftime('%D').replace('/','-') + '.png')
+    browser.get_screenshot_as_file()
+    browser.get('https://vaughanpl.bibliocommons.com/user/logout')
+    browser.sleep(3)
+
+
+def main():
+    browser = webdriver.Chrome(PATH_TO_CHROMEDRIVER)
+    browser.get('https://vaughanpl.bibliocommons.com/user/login')
+    
+    try:
+        for login_info in LOGIN_INFO:
+            login(browser, login_info)
+            renew(browser, login_info)
+    except:
+        print('An error occured. Probably due to slow response from website. Try increasing value "TIME_FACTOR" variable.') 
+    
+    browser.close()
+    
+
+if __name__ == "__main__":
+    main()
+
+
